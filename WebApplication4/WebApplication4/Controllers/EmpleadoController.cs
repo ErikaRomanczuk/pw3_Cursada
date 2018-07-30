e4453b88b930c30bb9607c79bd12ba13d036ebaa
Erika Romanczuk <31408057+ErikaRomanczuk@users.noreply.github.com>
2018-07-30 10:45:26 -0300

    Add files via upload

---
+++ WebApplication4/WebApplication4/Controllers/EmpleadoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        // GET: Empleado
+        public ActionResult EmpleadoDelMes()
+        {
+            EmpleadoModel empleado = new EmpleadoModel();
+
+            empleado.Nombre = "Cristiano";
+            empleado.Apellido = "Ronaldo";
+            empleado.Edad = 32;
+            empleado.Antiguedad = 12;
+
+            return View(empleado);
+        }
+
+        public ActionResult Alta()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Alta(EmpleadoModel empleado)
+        {
+            string nombreBindAutomatico = empleado.Nombre;
+            int edadBindAutomatico = empleado.Edad;
+/*
+            string Nombre = Request["Nombre"];
+            string Apellido = Request["Apellido"];
+            string Edad = Request["Edad"];
+            string Antiguedad = Request["Antiguedad"];*/
+            return View("EmpleadoDelMes", empleado);
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/Models/EmpleadoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class EmpleadoModel
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public int Antiguedad { get; set; }
+
+    }
+}
