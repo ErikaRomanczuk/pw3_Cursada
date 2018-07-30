fefe27709e382dc302967e5056b29d1fab50b9b3
Erika Romanczuk <31408057+ErikaRomanczuk@users.noreply.github.com>
2018-07-30 10:46:42 -0300

    Add files via upload

---
+++ WebApplication5/WebApplication5/Controllers/IniciarAlumnoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class IniciarAlumnoController : Controller
+    {
+       // GET: IniciarAlumno
+        public ActionResult Alta()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Alta(AlumnoModel alumno)
+        {
+            string bindNombre = alumno.Nombre;
+            int bindDni = alumno.Dni;
+            int bindEdad = alumno.Edad;
+
+            return View("AltaExito", alumno);
+        }
+
+    }
+}
---
+++ WebApplication5/WebApplication5/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Class1
+    {
+        public string Alumno;
+        public int Dni;
+        public int Edad;
+    }
+}
