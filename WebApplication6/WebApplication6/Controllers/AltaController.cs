782217007e1e3505003412b8c7bd9a2e6a6889b2
Erika Romanczuk <31408057+ErikaRomanczuk@users.noreply.github.com>
2018-07-30 10:52:49 -0300

    Add files via upload

---
+++ WebApplication6/WebApplication6/Controllers/AltaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class AltaController : Controller
+    {
+        // GET: Alta
+        public ActionResult AltaAlumno()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AltaAlumno(AlumnoModel alumno)
+        {
+            string nombreBinding = alumno.Nombre;
+            string apellidoBinding = alumno.Apellido;
+            int DniBinding = alumno.Dni;
+            int EdadBinding = alumno.Edad;
+            return View("AltaConfirmacion", alumno);
+        }
+
+
+    }
+}
