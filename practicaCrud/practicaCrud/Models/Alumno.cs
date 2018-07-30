14a2ed362e68a58c71d89c12e99c961a21e04fc4
Erika Romanczuk <31408057+ErikaRomanczuk@users.noreply.github.com>
2018-07-30 11:09:03 -0300

    Add files via upload

---
+++ practicaCrud/practicaCrud/Models/Alumno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace practicaCrud.Models
+{
+    public class Alumno
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "El nombre {0} es incorrecto")]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido {0} es incorrecto")]
+        [Display(Name = "Apellido")]
+        public string Apellido { get; set; }
+    }
+}
---
+++ practicaCrud/practicaCrud/Repository/AlumnoRepository.cs
@@ -0,0 +1,59 @@
+﻿using practicaCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace practicaCrud.Repository
+{
+    public class AlumnoRepository
+    {
+        List<Alumno> listAlumno = new List<Alumno>();
+
+
+        public List<Alumno> listarAlumnos()
+        {
+            Alumno a1 = new Alumno();
+            Alumno a2 = new Alumno();
+            a1.Nombre = "Nombre 1";
+            a1.Apellido = "Apellido 1";
+            a1.id = 1;
+            a2.Nombre = "Nombre 2";
+            a2.Apellido = "Apellido 2";
+            a2.id = 2;
+            listAlumno.Add(a1);
+            listAlumno.Add(a2);
+
+            return listAlumno;
+
+        }
+
+        public void crearAlumno (Alumno alumno)
+        {
+            listAlumno.Add(alumno);
+
+        }
+
+        public Alumno buscarAlumnoPorId (int id)
+        {
+         //   List<Alumno> listAl = listarAlumnos();
+
+            foreach(var al in listarAlumnos())
+            {
+                if (id == al.id)
+                {
+                    return al;
+                }
+            }
+
+            throw new Exception("Alumno inexistente");
+        }
+
+        public void Modificar (Alumno alumno)
+        {
+            Alumno aluModi = buscarAlumnoPorId(alumno.id);
+            aluModi.Nombre = alumno.Nombre;
+            aluModi.Apellido = alumno.Apellido;
+        }
+    }
+}
